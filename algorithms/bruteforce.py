1a0e8f66c3a455482d923c7da27c9879b04b1905
MiladEzame <milad.ezame@gmail.com>
2021-09-26 19:02:28 +0200

    saving before optimization tests

--- algorithms/bruteforce.py
+++ algorithms/bruteforce.py
@@ -1,5 +1,6 @@
 import csv
 import itertools
+import time
 
 
 def permutations():
@@ -8,23 +9,34 @@ def permutations():
         actions = []
         actions_finales = []
         for row in csvReader:
+            # O(n) search
             actions.append(row)
+            # O(n) insert
         global_list = []
         for i in range(1, len(actions) + 1):
+            # O(n^3) search
             combi = itertools.combinations(actions, i)
+            # O(n!)
             for iter in combi:
+                # O(n^2)
                 actions_selectionnes = []
                 valo = 0
                 cout = 0
                 for act in iter:
+                    # O(n) search
                     actions_selectionnes.append(act[0])
+                    # O(n) insert
                     cout += int(act[1])
                     valo += int(act[1]) * float(act[2])
                 global_list.append((actions_selectionnes, cout, cout + valo))
+                # O(n) insert
         for actions in global_list:
+            # O(n)
             if(actions[1] <= 500):
                 actions_finales.append(actions)
+                # O(n) insert
         benef_max = sorted(actions_finales, key=lambda x: x[2], reverse=True)
+        # O(nlog(n))
         return benef_max[0]
         # faire une diapo pour l'analyse du code et expliquer les modifs
         # ameliorations / limite / vérifications des points limites
@@ -32,5 +44,7 @@ def permutations():
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     meilleure_action = permutations()
     print(meilleure_action)
+    print("--- executed in {} seconds ---".format(time.time() - start_time))
