8e1ec60af30df7f76b835609f2771985aba11fb5
MiladEzame <milad.ezame@gmail.com>
2021-09-28 11:01:15 +0200

    test remplissage matrice

--- algorithms/optimized.py
+++ algorithms/optimized.py
@@ -15,19 +15,16 @@ def read_actions():
 
 
 def creation_matrice(LIMIT, all_actions):
-    matrice = [[0 for x in range((LIMIT))] for x in range(len(all_actions))]
 
-    for action in range(1, len(all_actions)):
-        print(action)
+    matrice = [[0 for x in range(LIMIT+1)] for x in range(len(all_actions) + 1)]
 
-
-# n actions (lignes) et 0 à coût max(colonnes)
-# Remplissage de la matrice :
-# Pour chaque lignes de la matrice entre 0 et n il y a l'action (nom, cout, benef)
-# Chaque colonnes de ma matrice contiendra une valeur entre 0 et mon coût maximum
-# Remplir chaque cellule de ma matrice avec les coûts correspondants
-# Comparer chaque cellule avec la précédente pour voir si elle est plus profitable que la précédente
-# Récupérer la meilleure valeur et la renvoyer
+    for i in range(1, len(all_actions) + 1):
+        for p in range(1, LIMIT + 1):
+            if int(all_actions[i-1][1]) <= p:
+                matrice[i][p] = max((float(all_actions[i-1][2]) * int(all_actions[i-1][1]) +
+                                     int(all_actions[i-1][1])), matrice[i-1][p])
+            else:
+                matrice[i][p] = matrice[i-1][p]
 
 
 if __name__ == "__main__":
