6c3f49c7a05af646d09428dec17be341369b9310
MiladEzame <milad.ezame@gmail.com>
2021-10-05 23:10:46 +0200

    algo optimized and bruteforce ok

--- algorithms/bruteforce.py
+++ algorithms/bruteforce.py
@@ -3,40 +3,42 @@ import itertools
 import time
 
 
-def permutations():
+def read_csv():
     with open("actions.csv", "r") as action:
         csvReader = csv.reader(action, delimiter=";")
         actions = []
-        actions_finales = []
         for row in csvReader:
             # O(n) search
             actions.append(row)
-            # O(n) insert
-        for i in range(1, len(actions) + 1):
-            # O(n^3) search
-            combi = itertools.combinations(actions, i)
-            # O(n!)
-            for iter in combi:
-                # O(n^2)
-                valo = 0
-                cout = 0
-                selected_actions = []
-                for act in iter:
-                    # O(n) insert
-                    selected_actions.append(act[0])
-                    cout += int(act[1])
-                    valo += int(act[1]) * float(act[2])
-                if (cout <= 500):
-                    actions_finales.append((selected_actions, cout, cout + valo))
+    permutations(actions)
+
+
+def permutations(actions):
+    actions_finales = []
+    for i in range(1, len(actions) + 1):
+        # O(n^3) search
+        combi = itertools.combinations(actions, i)
+        # O(n!)
+        for iter in combi:
+            # O(n^2)
+            valo = 0
+            cout = 0
+            selected_actions = []
+            for act in iter:
                 # O(n) insert
+                selected_actions.append(act[0])
+                cout += int(act[1])
+                valo += int(act[1]) * float(act[2])
+            if (cout <= 500):
+                actions_finales.append((selected_actions, cout, cout + valo))
+            # O(n) insert
         # O(n) insert
         benef_max = sorted(actions_finales, key=lambda x: x[2], reverse=True)
         # O(nlog(n))
-        return benef_max[0]
+    print(benef_max[0])
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    meilleure_action = permutations()
-    print(meilleure_action)
+    read_csv()
     print("--- executed in {} seconds ---".format(time.time() - start_time))
--- algorithms/optimized.py
+++ algorithms/optimized.py
@@ -10,42 +10,91 @@ def read_actions():
         csvReader = csv.reader(action, delimiter=";")
         actions = []
         for row in csvReader:
-            actions.append((row[0], row[1], row[2]))
+            if(float(row[1]) >= 0):
+                actions.append((row[0], float(row[1]), float(row[2])))
         creation_matrice(LIMIT, actions)
+
+
+def read_actions_entiers():
+    with open("dataset1.csv", "r") as action:
+        csvReader = csv.reader(action, delimiter=";")
+        actions = []
+        for row in csvReader:
+            if(float(row[1]) > 0):
+                actions.append((row[0], round(float(row[1])*100), float(row[2])/100))
+        creation_matrice_datasets(LIMIT, actions)
 
 
 def creation_matrice(LIMIT, all_actions):
 
-    matrice = [[0 for x in range(LIMIT+1)] for x in range(len(all_actions) + 1)]
+    matrice = [[0 for x in range(LIMIT + 1)] for x in range(len(all_actions) + 1)]
     for i in range(1, len(all_actions) + 1):
         for c in range(1, LIMIT + 1):
-            if int(float(all_actions[i-1][1])) <= c:
-                matrice[i][c] = max(float(all_actions[i-1][2]) * float(all_actions[i-1][1]) +
-                                    matrice[i-1][c-(int(float(all_actions[i-1][1])))], matrice[i-1][c])
-                # pourquoi INDEX OUT OF RANGE dans le fichier dataset1?
-                # int(float(Str)) pour éviter les erreurs de typage & d'indice
+            if int(all_actions[i-1][1]) <= c:
+                matrice[i][c] = max(all_actions[i-1][2] * all_actions[i-1][1] +
+                                    matrice[i-1][c-(int(all_actions[i-1][1]))], matrice[i-1][c])
             else:
                 matrice[i][c] = matrice[i-1][c]
     c = LIMIT
     n = len(all_actions)
     actions_selectionnees = []
 
-    while c >= 0 and n >= 0:
+    while c >= 0 and n > 0:
         a = all_actions[n-1]
-        # print(matrice[n][c])
-        # print(int(float(matrice[n-1][c-(int(float(a[1])))])) + (float(a[2]) * float(a[1])))
-        if matrice[n][c] == int(float(matrice[n-1][c-(int(float(a[1])))])) + (float(a[2]) * float(a[1])):
-            actions_selectionnees.append(a)
-            c -= int(float(a[1]))
-
+        try:
+            if matrice[n][c] == matrice[n-1][c-(int(a[1]))] + a[2] * a[1]:
+                actions_selectionnees.append(a)
+                c -= int(a[1])
+        except IndexError:
+            print(len(matrice))
+            print(a, c, n)
+            raise
         n -= 1
+    print("ACTIONS SELECTIONNEES :")
+    cout = 0
+    for action in actions_selectionnees:
+        print(action[0])
+        cout += action[1]
+    print("COUT TOTAL : {}".format(cout))
+    print("PROFIT TOTAL : {:.2f}".format(matrice[-1][-1]))
 
-    print(round(matrice[-1][-1], 2))
-    print("ACTIONS SELECTED")
-    print(actions_selectionnees)
+
+def creation_matrice_datasets(LIMIT, all_actions):
+    LIMIT *= 100
+    matrice = [[0 for x in range(LIMIT + 1)] for x in range(len(all_actions) + 1)]
+    for i in range(1, len(all_actions) + 1):
+        for c in range(1, LIMIT + 1):
+            if int(all_actions[i-1][1]) <= c:
+                matrice[i][c] = max(all_actions[i-1][2] * all_actions[i-1][1] +
+                                    matrice[i-1][c-(int(all_actions[i-1][1]))], matrice[i-1][c])
+            else:
+                matrice[i][c] = matrice[i-1][c]
+    c = LIMIT
+    n = len(all_actions)
+    actions_selectionnees = []
+
+    while c >= 0 and n > 0:
+        a = all_actions[n-1]
+        try:
+            if matrice[n][c] == matrice[n-1][c-(int(a[1]))] + a[2] * a[1]:
+                actions_selectionnees.append((a[0], a[1]/100, a[2]))
+                c -= int(a[1])
+        except IndexError:
+            print(len(matrice))
+            print(a, c, n)
+            raise
+        n -= 1
+    print("ACTIONS SELECTIONNEES : ")
+    cout = 0
+    for action in actions_selectionnees:
+        print("- {}".format(action[0]))
+        cout += action[1]*100
+    print("COUT TOTAL : {}".format(cout/100))
+    print("PROFIT TOTAL : {}".format(round(matrice[-1][-1])/100))
 
 
 if __name__ == "__main__":
     start_time = time.time()
     read_actions()
+    # read_actions_entiers()
     print("--- executed in {} seconds ---".format(time.time() - start_time))
