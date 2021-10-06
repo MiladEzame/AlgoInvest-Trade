c25bbe1a0ed16cf971a9a1c7583f3c3e97038fcc
MiladEzame <milad.ezame@gmail.com>
2021-10-06 08:38:30 +0200

    projet fini

--- algorithms/bruteforce.py
+++ algorithms/bruteforce.py
@@ -8,7 +8,6 @@ def read_csv():
         csvReader = csv.reader(action, delimiter=";")
         actions = []
         for row in csvReader:
-            # O(n) search
             actions.append(row)
     permutations(actions)
 
@@ -16,25 +15,18 @@ def read_csv():
 def permutations(actions):
     actions_finales = []
     for i in range(1, len(actions) + 1):
-        # O(n^3) search
         combi = itertools.combinations(actions, i)
-        # O(n!)
         for iter in combi:
-            # O(n^2)
             valo = 0
             cout = 0
             selected_actions = []
             for act in iter:
-                # O(n) insert
                 selected_actions.append(act[0])
                 cout += int(act[1])
                 valo += int(act[1]) * float(act[2])
             if (cout <= 500):
                 actions_finales.append((selected_actions, cout, cout + valo))
-            # O(n) insert
-        # O(n) insert
         benef_max = sorted(actions_finales, key=lambda x: x[2], reverse=True)
-        # O(nlog(n))
     print(benef_max[0])
 
 
@@ -42,3 +34,19 @@ if __name__ == "__main__":
     start_time = time.time()
     read_csv()
     print("--- executed in {} seconds ---".format(time.time() - start_time))
+
+
+# création matrice:
+# matrice dont le nombre de lignes est égal au nombre d'actions
+# matrice dont le nombre de colonnes est égal au cout maximum (ici 500)
+
+# initialisation de la matrice avec des 0
+
+# pour mes actions comprises entre 0 et nombre d'actions (parcours les lignes)
+# pour mes couts compris entre 0 et cout maximum (parcours les colonnes)
+# si le cout (ou la somme des couts) de mon action est inférieur ou égale à mon cout max:
+# l'élément dans ma matrice en case [action actuelle][cout actuel] sera égal au maximum entre
+# le calcul de ma valorisation sur 2 ans ET la valorisation pour ce même cout de ma ligne précédente.
+
+# si mon prix est supérieur à mon cout maximum:
+# insertion de la valorisation précédente dans le tableau
